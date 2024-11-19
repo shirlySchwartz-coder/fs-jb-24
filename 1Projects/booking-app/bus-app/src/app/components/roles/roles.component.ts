b6be5639d0de253db4b50fa34e5d7dc34e485f53
shirly <s0577611175@gmail.com>
2024-11-19 15:41:34 +0200

    form&data-binding

--- 1Projects/booking-app/bus-app/src/app/components/roles/roles.component.ts
+++ 1Projects/booking-app/bus-app/src/app/components/roles/roles.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
-  styleUrl: './roles.component.css'
+  styleUrl: './roles.component.css',
 })
 export class RolesComponent {
-  firstName: string ="Angular Bus App";
-  angularVersion = "Version 18.2"
+  firstName: string = 'Angular Bus App';
+  angularVersion = 'Version 18.2';
+  version: number = 18;
+  currentDate: Date = new Date();
+  inputType: string = 'button';
+  state: string = '';
+
+  // create functions
+  showWelcomeAlert(){
+    //normal function
+    alert('Welcome to Angular 18')
+  }
+showMessage(message:string){
+  alert(message)
+}
+
 }
